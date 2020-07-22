698a5220ba2873d17b609450e7e92f48567ee09b
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2020-07-22 18:27:40 -0400

    Cleaning AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductsService } from 'src/app/providers/services/products.service';
-import { Product } from 'src/app/providers/models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -9,58 +7,8 @@ import { Product } from 'src/app/providers/models/product.model';
 })
 export class AppComponent implements OnInit {
 
-  allProducts: Product[] = [];
-  paginatedProducts: Product[] = [];
-  page_size: number = 9;
-  page_number: number = 1;
-  canShowNextPageButton: boolean;
-
-  constructor(
-    private service: ProductsService
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getDataFromAPI();
-  }
-
-  private getDataFromAPI() {
-    this.service.getProducts().subscribe(
-      success => {
-        this.allProducts = Object.values(success.products);
-        this.initFirstPage();
-      }
-    );
-  }
-
-  private initFirstPage() {
-    this.paginatedProducts = this.paginateArray();
-
-    this.canShowNextPageButton = this.paginatedProducts.length > 0 ? true : false;
-  }
-
-  public nextPage() {
-    this.page_number += 1;
-    Array.prototype.push.apply(this.paginatedProducts, this.paginateArray());
-
-    if (!this.hasNextPage()) {
-      this.canShowNextPageButton = false;
-    }
-  }
-
-  private hasNextPage(): boolean {
-    const fakeNextPage = this.page_number + 1;
-    const fakeNextPageProducts = this.allProducts.slice((fakeNextPage - 1) * this.page_size, fakeNextPage * this.page_size);
-
-    if (fakeNextPageProducts.length == 0) {
-      this.canShowNextPageButton = false;
-      return false;
-    } else {
-      this.canShowNextPageButton = true;
-      return true;
-    }
-  }
-
-  private paginateArray(): Product[] {
-    return this.allProducts.slice((this.page_number - 1) * this.page_size, this.page_number * this.page_size);
   }
 }
